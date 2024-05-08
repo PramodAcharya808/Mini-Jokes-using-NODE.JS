3877e4b8089faea39012384607e2390d5332a27a
Pramod_Acharya <pramodacharya808@gmail.com>
2024-05-08 10:53:29 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+const App = () => {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/jokes")
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Jokes Count: {jokes.length}</h1>
+      {jokes.map((jokes) => {
+        <div key={jokes.id}>
+          <h2>{jokes.title}</h2>
+          <p>{jokes.body}</p>
+        </div>;
+      })}
+    </div>
+  );
+};
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/jokes", (req, res) => {
+  const jokesList = [
+    { id: 1, title: "first Joke", body: "This is my first joke" },
+    { id: 2, title: "second Joke", body: "This is my second joke" },
+    { id: 3, title: "third Joke", body: "This is my third joke" },
+    { id: 4, title: "fourth Joke", body: "This is my fourth joke" },
+    { id: 5, title: "fifth Joke", body: "This is my fifth joke" },
+    { id: 6, title: "sixth Joke", body: "This is my sixth joke" },
+    { id: 7, title: "seventh Joke", body: "This is my first joke" },
+    { id: 8, title: "eighth Joke", body: "This is my eighth joke" },
+  ];
+
+  res.send(jokesList);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port} port`);
+});
